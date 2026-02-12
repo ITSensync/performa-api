07d14101b2c61e0c7cfea42149fd16bed0f00e75
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2026-02-12 16:24:57 +0700

    fix: db host

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',   // WAJIB localhost
-  port: 1404,          // PORT LOKAL SSH TUNNEL
+  // host: '127.0.0.1',   // WAJIB localhost
+  // port: 1404,          // PORT LOKAL SSH TUNNEL
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
